0425a5e1c955c4e0f19591e20a4d49f2917731c7
echo <play3step@naver.com>
2023-06-26 13:49:54 +0900

    myPortfolio

--- test.js
+++
@@ -1,8 +0,0 @@
-const obj = {name: 'Tom'};
-
-const say = function(city){
-    console.log('name is '+ this.name + ' in ' + city);
-}
-
-const boundSay = say.bind(obj);
-boundSay("seoul");  //name is Tom in seoul
